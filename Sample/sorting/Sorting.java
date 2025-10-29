83f3e5c4b5aa6246a7f00550b992887609024f94
wpgoble <will.goble@gmail.com>
2025-10-29 09:12:16 -0400

    adds insertion sort

--- Sample/sorting/Sorting.java
+++ Sample/sorting/Sorting.java
@@ -38,6 +38,22 @@ public class Sorting {
         }
     }
 
+    static void InsertionSort(int[] array) {
+        int size = array.length;
+        for (int i = 1; i < size; i++) {
+            int key = array[i];
+            int j = i - 1;
+
+            // Move elements of array[0..i-1], that are greater than key,
+            // to one position ahead of their current position
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
+    }
+
     public static void main(String[] args) {
         int[] array = {64, 36, 25, 12, 22, 11, 90};
         int size = array.length;
